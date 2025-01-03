db470036df14217676441e9a1ce503be7fa938c3
Adalton Júnio <juniotsl@gmail.com>
2025-01-03 15:10:41 -0300

    teste2

--- solutions/express/api/index.js
+++ solutions/express/api/index.js
@@ -1,39 +1,68 @@
-require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: false, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-const express = require('express');
 const app = express();
-const { sql } = require('@vercel/postgres');
+app.use(cors(corsOptions));
+app.use(express.json());
 
-const bodyParser = require('body-parser');
-const path = require('path');
+const PORT = 3000;
 
-// Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.post("/api", (req, res) => {
+  let data = JSON.stringify({
+    processing_mode: "automatic",
+    external_reference: "ext_ref_1234",
+    description: "order description",
+    marketplace: "NONE",
+    marketplace_fee: "1.00",
+    total_amount: "100.00",
+    expiration_time: "P3Y6M4DT12H30M5S",
+    type: "online",
+    payer: { ...req.body.payer },
+    transactions: { ...req.body.transactions },
+    items: [
+      {
+        title: "title",
+        description: "descriptions",
+        unit_price: "10.00",
+        id: "ABC",
+        category_id: "category",
+        picture_url: "https://www.mercadopago.com/img",
+        quantity: 1,
+      },
+    ],
+  });
 
-app.use(express.static('public'));
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://api.mercadopago.com/v1/orders",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Idempotency-Key": Math.random(),
+      Authorization: `Bearer ${process.env.PK}`,
+    },
+    data: data,
+  };
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(response.data);
+      res.status(response.status).json(response.data);
+    })
+    .catch(() => {
+      res.status(500);
+    });
 });
 
-app.get('/api', function (req, res) {
-    res.status(200).send(process.env.PK)
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
-app.get('/uploadUser', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
-});
-
-app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
-	try {
-		await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
-		res.status(200).send('<h1>User added successfully</h1>');
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error adding user');
-	}
-});
-
-app.listen(3000, () => console.log('Server ready on port 3000.'));
-
-module.exports = app;
+console.log("Hello CodeSandbox");
