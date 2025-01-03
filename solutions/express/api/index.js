bb6193651f6f43a4c319db84b0a93499b14d80fb
Adalton Júnio <juniotsl@gmail.com>
2025-01-03 15:09:13 -0300

    teste

---
+++ solutions/express/api/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const { sql } = require('@vercel/postgres');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+});
+
+app.get('/api', function (req, res) {
+    res.status(200).send(process.env.PK)
+});
+
+app.get('/uploadUser', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
+});
+
+app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
+	try {
+		await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
+		res.status(200).send('<h1>User added successfully</h1>');
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error adding user');
+	}
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
